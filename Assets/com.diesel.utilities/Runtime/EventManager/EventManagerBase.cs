5837b8b4bb8d146ca26517a5454698abe9a171c2
Andries <andriesdg199@gmail.com>
2023-05-12 13:50:23 +0200

    chore: cleanup on Event Manager

--- Assets/com.diesel.utilities/Runtime/EventManager/EventManagerBase.cs
+++ Assets/com.diesel.utilities/Runtime/EventManager/EventManagerBase.cs
@@ -1,46 +1,42 @@
 using System.Collections.Generic;
 
-public enum EventType {
+public static class EventManager<Enum> {
+    private static readonly Dictionary<Enum, System.Action> eventActions = new();
 
-}
-
-public static class EventManager {
-    private static readonly Dictionary<EventType, System.Action> eventActions = new();
-
-    public static void Subscribe(EventType eventType, System.Action eventToSubscribe) {
+    public static void Subscribe(Enum eventType, System.Action eventToSubscribe) {
         if (!eventActions.ContainsKey(eventType))
             eventActions.Add(eventType, null);
 
         eventActions[eventType] += eventToSubscribe;
     }
 
-    public static void Unsubscribe(EventType eventType, System.Action eventToUnsubscribe) {
+    public static void Unsubscribe(Enum eventType, System.Action eventToUnsubscribe) {
         if (eventActions.ContainsKey(eventType)) 
             eventActions[eventType] -= eventToUnsubscribe;
     }
 
-    public static void Invoke(EventType eventType) {
+    public static void Invoke(Enum eventType) {
         if (eventActions.ContainsKey(eventType))
             eventActions[eventType]?.Invoke();
     }
 }
 
-public static class EventManager<T> {
-    private static readonly Dictionary<EventType, System.Action<T>> eventActions = new();
+public static class EventManagerBase<Enum, T> {
+    private static readonly Dictionary<Enum, System.Action<T>> eventActions = new();
 
-    public static void Subscribe(EventType eventType, System.Action<T> eventToSubscribe) {
+    public static void Subscribe(Enum eventType, System.Action<T> eventToSubscribe) {
         if (!eventActions.ContainsKey(eventType))
             eventActions.Add(eventType, null);
 
         eventActions[eventType] += eventToSubscribe;
     }
 
-    public static void Unsubscribe(EventType eventType, System.Action<T> eventToUnsubscribe) {
+    public static void Unsubscribe(Enum eventType, System.Action<T> eventToUnsubscribe) {
         if (eventActions.ContainsKey(eventType)) 
             eventActions[eventType] -= eventToUnsubscribe;
     }
 
-    public static void Invoke(EventType eventType, T obj) {
+    public static void Invoke(Enum eventType, T obj) {
         if (eventActions.ContainsKey(eventType))
             eventActions[eventType]?.Invoke(obj);
     }
